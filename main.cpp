97d8de0849c3dc320cf6d071cf109927ef5fe07c
zyhwan <zyhwan010410@naver.com>
2026-03-20 17:49:50 +0900

    .

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include<iostream>
 
-using namespace std;
-
 class String
 {
 public:
@@ -12,26 +10,61 @@ public:
 	}
 	String(const char* str)
 	{
+		len = strlen(str);
+		Str = new char[len + 1];
+		strcpy(Str, str);
+	}
+	//복사 생성
+	String(const String& rhs)
+	{
+		len = rhs.len;
+		Str = new char[len + 1];
+		strcpy(Str, rhs.Str);
+	}
+	//복사 대입 연산자 오버로딩
+	String& operator=(String& rhs)
+	{
+		if (this != &rhs)
+		{
+			len = rhs.len;
+			delete[] Str;
+			Str = new char[len + 1];
+			strcpy(Str, rhs.Str);
+		}
+		return *this;
+	}
 
+	const char* GetStr()
+	{
+		if (len > 0)
+		{
+			return Str;
+		}
+		return "";
 	}
 
 	~String()
 	{
-
+		std::cout << "소멸자 호출" << std::endl; 
+		delete Str;
 	}
-
-
-
 private:
 	char* Str;
 	int len;
-
 };
 
 
 int main()
 {
+	String str("Hello");
+	String s2(str);
+	String s3;
 
+	s3 = s2;
+
+	std::cout << str.GetStr() << std::endl;
+	std::cout << s2.GetStr() << std::endl;
+	std::cout << s3.GetStr() << std::endl;
 
 	return 0;
 }
