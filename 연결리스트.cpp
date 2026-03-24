05647f3ec22c776fe0d5b48203a3fe533f006ad7
zyhwan <zyhwan010410@naver.com>
2026-03-24 17:20:58 +0900

    insertFront 연결리스트 연습

--- 연결리스트.cpp
+++ 연결리스트.cpp
@@ -1,56 +1,58 @@
-#include <iostream>
+#include<iostream>
 
-// 1. 노드 구조체 정의
-struct Node {
-    int data;
-    Node* next;
+struct Node
+{
+	int data;
+	Node* next;
 
-    // 생성자
-    Node(int val) : data(val), next(nullptr) {}
+	Node(int value) : data(value), next(nullptr) { }
 };
 
-// 2. 연결 리스트 클래스
-class LinkedList {
+class LinkedList
+{
 private:
-    Node* head; // 시작 노드
-
+	Node* Head;
 public:
-    LinkedList() : head(nullptr) {}
+	LinkedList() : Head(nullptr)
+	{
 
-    // 맨 앞에 노드 삽입
-    void insertFront(int val) {
-        Node* newNode = new Node(val);
-        newNode->next = head;
-        head = newNode;
-    }
-
-    // 리스트 출력
-    void display() {
-        Node* temp = head;
-        while (temp != nullptr) {
-            std::cout << temp->data << " -> ";
-            temp = temp->next;
-        }
-        std::cout << "NULL" << std::endl;
-    }
-
-    // 소멸자 (메모리 해제)
-    ~LinkedList() {
-        Node* current = head;
-        while (current != nullptr) {
-            Node* next = current->next;
-            delete current;
-            current = next;
-        }
-    }
+	}
+	~LinkedList()
+	{
+		Node* current = Head;
+		while (current != nullptr)
+		{
+			Node* next = current->next;
+			delete current;
+			current = next;
+		}
+	}
+	void AddNode(int value)
+	{
+		Node* newNode = new Node(value);
+		newNode->next = Head;
+		Head = newNode;
+	}
+	void Display()
+	{
+		Node* temp = Head;
+		while (temp != nullptr)
+		{
+			std::cout << temp->data << std::endl;
+			temp = temp->next;
+		}
+	}
 };
 
-int main() {
-    LinkedList list;
-    list.insertFront(3);
-    list.insertFront(2);
-    list.insertFront(1);
 
-    list.display(); // 출력: 1 -> 2 -> 3 -> NULL
-    return 0;
+int main()
+{
+	LinkedList L;
+
+	L.AddNode(3);
+	L.AddNode(2);
+	L.AddNode(1);
+
+	L.Display();
+
 }
